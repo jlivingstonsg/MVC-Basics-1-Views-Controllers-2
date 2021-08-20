895c8d4fa912d808e602772b5c9a5d0cf19a1a53
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-08-20 14:15:46 +0200

    Doctor Cotroller and view

---
+++ CodeAlong Template/Controllers/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAlong_Template.Controllers
+{
+    public class Doctor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
