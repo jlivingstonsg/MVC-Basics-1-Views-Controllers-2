b95ef082f08bc41a03e9757eae0c777f796d6dac
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-08-20 13:25:30 +0200

    Probably done now....

---
+++ CodeAlong Template/Controllers/Projects.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAlong_Template.Controllers
+{
+    public class Projects : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
