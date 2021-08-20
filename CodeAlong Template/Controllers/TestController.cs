341ec5a8329cd63989ba49a882319e6ee54e1a88
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-08-20 09:59:31 +0200

    Add project files.

---
+++ CodeAlong Template/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAlong_Template.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CustomTest()
+        {
+            return View(); 
+        }
+    }
+}
